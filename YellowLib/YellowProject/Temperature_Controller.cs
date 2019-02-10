26f006160679abbbf344de7cae8b55e4da443aa9
ADJUA A. ANSARI <ansari_a@lynchburg.edu>
2019-02-10 16:28:13 -0500

    change temp from C ->F and vice-versa

--- YellowLib/YellowProject/Temperature_Controller.cs
+++ YellowLib/YellowProject/Temperature_Controller.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,30 @@ namespace YellowProject
             return id_;
         }
 
+        //public int getTemp()
+        //{
+        //    return CurrentTemp_;
+        //}
+
+        public void changeTemp(float temp, char conv)
+        {
+            CurrentTemp_ = temp;
+            if(char.ToUpper(prefConv_) == 'C' &&
+                char.ToUpper(conv) == 'F')
+            {
+                CurrentTemp_ = (CurrentTemp_ * 1.8) + 32;
+            }
+            else if(char.ToUpper(prefConv_) == 'F' &&
+                char.ToUpper(conv) == 'C')
+            {
+                CurrentTemp_ = (9.5) * (CurrentTemp_ + 32);
+            }
+        }
+
         private int id_ { get; set; }
+
+        private char prefConv_ { get; set; }
+        private double CurrentTemp_ { get; set; }
+
     }
 }
