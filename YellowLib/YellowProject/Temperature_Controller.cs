05ad65dcf0eda654d8fd361e01fe8ffa8d1f7a7b
ADJUA A. ANSARI <ansari_a@lynchburg.edu>
2019-02-11 09:51:03 -0500

    test for tempChange

--- YellowLib/YellowProject/Temperature_Controller.cs
+++ YellowLib/YellowProject/Temperature_Controller.cs
@@ -14,6 +14,8 @@ namespace YellowProject
         public Temperature_Controller(int id)
         {
             id_ = id;
+            CurrentTemp_ = 0;
+            PrefConv_ = 'C';
         }
 
         public int getUserID()
@@ -29,22 +31,23 @@ namespace YellowProject
         public void changeTemp(float temp, char conv)
         {
             CurrentTemp_ = temp;
-            if(char.ToUpper(prefConv_) == 'C' &&
+            if(char.ToUpper(PrefConv_) == 'C' &&
                 char.ToUpper(conv) == 'F')
             {
                 CurrentTemp_ = (CurrentTemp_ * 1.8) + 32;
             }
-            else if(char.ToUpper(prefConv_) == 'F' &&
+            else if(char.ToUpper(PrefConv_) == 'F' &&
                 char.ToUpper(conv) == 'C')
             {
                 CurrentTemp_ = (9.5) * (CurrentTemp_ + 32);
             }
+
+            PrefConv_ = char.ToUpper(conv);
         }
 
         private int id_ { get; set; }
-
-        private char prefConv_ { get; set; }
-        private double CurrentTemp_ { get; set; }
+        public char PrefConv_ { get; private set; }
+        public double CurrentTemp_ { get; private set; }
 
     }
 }
--- YellowLib/YellowTestProject/UnitTest1.cs
+++ YellowLib/YellowTestProject/UnitTest1.cs
@@ -17,5 +17,17 @@ namespace YellowTestProject
             Assert.AreEqual(id, test_cont.getUserID());
         }
 
+
+        [TestMethod]
+        public void changeTemp()
+        {
+            int id = 123456;
+            int temp = 30;
+            char tempType = 'c';
+            Temperature_Controller test_temp = new Temperature_Controller(id);
+            test_temp.changeTemp(32, 'f');
+            Assert.AreEqual(test_temp.CurrentTemp_, 89.6);
+            Assert.AreEqual(test_temp.PrefConv_, 'F');
+        }
     }
 }
