de0d254d383f4964fd84990c8e26cf0543d3ba13
Nick Willertz <willertz_n@lynchburg.edu>
2019-02-19 10:03:48 -0500

    start for league manager project

--- YellowLib/YellowTestProject/UnitTest1.cs
+++ YellowLib/YellowTestProject/UnitTest1.cs
@@ -8,26 +8,7 @@ namespace YellowTestProject
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void HVAC_Test()
-        {
-            int id = 123456;
-            Temperature_Controller test_cont = new Temperature_Controller(id);
-
-            Assert.AreEqual(id, test_cont.getUserID());
-        }
 
 
-        [TestMethod]
-        public void changeTemp()
-        {
-            int id = 123456;
-            int temp = 30;
-            char tempType = 'c';
-            Temperature_Controller test_temp = new Temperature_Controller(id);
-            test_temp.changeTemp(32, 'f');
-            Assert.AreEqual(test_temp.CurrentTemp_, 89.6);
-            Assert.AreEqual(test_temp.PrefConv_, 'F');
-        }
     }
 }
